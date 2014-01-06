d20425cc2628ffe8d46bfff228f7e3a1b2f0c6f9
saerem21 <nemessisdds>
2014-01-06 17:25:55 +0900

    테이블뷰 삭제 예제 입니다..
    178page

--- Ex178_ex12/ViewController.m
+++ Ex178_ex12/ViewController.m
@@ -7,17 +7,49 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#define CELL_ID @"CELL_ID"
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+{
+    NSMutableArray *data;
+}
 
 @end
 
 @implementation ViewController
 
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    [data removeObjectAtIndex:indexPath.row];
+    NSArray *rows = [NSArray arrayWithObject:indexPath];
+    [tableView deleteRowsAtIndexPaths:rows withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"select Row %d",indexPath.row);
+}
+
+- (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"악세사리 버튼 눌림 %d",indexPath.row );
+}
+
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    cell.textLabel.text = data[indexPath.row];
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [data count];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    data =[[NSMutableArray alloc]initWithObjects:@"a",@"b",@"c",@"d",@"e",@"f", nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +59,4 @@
 }
 
 @end
+
